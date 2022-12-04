0f8d91979383f6304ba681646cfdac3731f036c9
mirzataha3 <96594428+mirzataha3@users.noreply.github.com>
2022-12-04 14:27:03 +0500

    Add files via upload

---
+++ Assignment 1.1.1.vb
@@ -0,0 +1,44 @@
+﻿Module Module1
+
+    Sub Main()
+        'DECLARATION
+
+        Dim s, first, last As String
+        Dim a, b, i As Integer
+        Dim thischar As Char
+        'INITIALISATION
+
+        s = " "
+        first = " "
+        last = " "
+        a = 0
+        b = 0
+        i = 0
+        thischar = " "
+
+        'INPUT
+
+        Console.Write("Enter the statement: ")
+        s = Console.ReadLine
+
+        'PROCESS
+
+
+        a = InStr(s, "not")
+        b = InStr(s, "bad")
+
+        For i = 1 To (a - 1)
+            thischar = Mid(s, i, 1)
+            first = first + thischar
+        Next
+
+        For i = (b + 3) To Len(s)
+            thischar = Mid(s, i, 1)
+            last = last + thischar
+        Next
+
+        Console.WriteLine("New statement: " & first & "good" & last)
+        Console.ReadKey()
+    End Sub
+
+End Module
---
+++ Assignment 1.4.vb
@@ -0,0 +1,49 @@
+﻿Module Module1
+
+    Sub Main()
+        'DECLARATION
+
+        Dim i, c, a, max As Integer
+        Dim thischar, thischar2, b As Char
+        Dim s As String
+
+        'INITIALISATION
+
+        i = 1
+        c = 1
+        a = 0
+        max = 0
+        thischar = " "
+        thischar2 = " "
+        b = " "
+        s = " "
+
+        'INPUT
+
+        Console.Write("Enter the statement: ")
+        s = Console.ReadLine
+
+        'PROCESS
+
+        For i = 1 To Len(s)
+            thischar = Mid(s, i, 1)
+            a = 0
+            For c = 1 To Len(s)
+                thischar2 = Mid(s, c, 1)
+                If thischar = thischar2 Then
+                    a = a + 1
+                End If
+            Next c
+            If a > max Then
+                max = a
+                b = thischar
+            End If
+        Next i
+
+        'OUTPUT
+
+        Console.WriteLine("Most repeated character is: " & b)
+        Console.ReadKey()
+    End Sub
+
+End Module
